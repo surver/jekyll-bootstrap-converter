78dd689d01ac221943dd571c938c7f4ac037cc53
Valentin Palacios <valentin.palacios@gmail.com>
2020-08-17 12:21:57 +0200

    change lib file distribution

---
+++ lib/jekyll-bootstrap-converter.rb
@@ -0,0 +1,9 @@
+require "jekyll/bootstrap/converter/version"
+require "jekyll/converters/bootstrap"
+
+module Jekyll
+  module BootstrapConverter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
